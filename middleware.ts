cbe98038ac0b2497db4eee61a66753d53d0e9dae
Gaetan Redoutez <gaetan.redoutez@preconys.com>
2025-08-21 09:55:01 +0200

    feat(middleware): update to protect admin

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,33 @@
 import createMiddleware from "next-intl/middleware";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { locales, defaultLocale } from "./i18n";
+import { getToken } from "next-auth/jwt";
 
-export default function middleware(request: NextRequest) {
-  // Handle i18n routing
-  const response = createMiddleware({
-    locales,
-    defaultLocale,
-    localeDetection: true,
-    localePrefix: "as-needed",
-  })(request);
+const intlMiddleware = createMiddleware({
+  locales,
+  defaultLocale,
+  localeDetection: true,
+  localePrefix: "as-needed",
+});
+
+export default async function middleware(request: NextRequest) {
+  const response = intlMiddleware(request);
+
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    const token = await getToken({
+      req: request,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+
+    if (!token) {
+      // Pas de session → redirige vers login
+      return NextResponse.redirect(new URL("/admin/login", request.url));
+    }
+  }
 
   return response;
 }
 
 export const config = {
-  // Match only internationalized pathnames
   matcher: ["/", "/(fr|en)/:path*", "/((?!_next|_vercel|.*\\..*).*)"],
 };
