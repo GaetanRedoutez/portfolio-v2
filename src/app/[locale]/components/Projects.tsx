f98778e9a0f2c8b40033ddc17663847a5d61db15
Gaetan Redoutez <gaetan.redoutez@preconys.com>
2025-09-10 09:17:03 +0200

    setup project section

--- src/app/[locale]/components/Projects.tsx
+++ src/app/[locale]/components/Projects.tsx
@@ -1,6 +1,23 @@
 import { SectionWrapper } from "@/app/components/ui/SectionWrapper";
+import { useTranslations } from "next-intl";
 import React from "react";
 
 export const Projects = () => {
-  return <SectionWrapper id="projects">Projects</SectionWrapper>;
+  const t = useTranslations("projects");
+
+  return (
+    <SectionWrapper
+      id="projects"
+      className="bg-gradient-to-b from-accent-10/95 to-accent-10/85"
+      innerClassName="flex flex-col w-full h-screen py-2 lg:py-8"
+    >
+      <h2
+        className="relative animate-fade-in-up-delay-3 text-center inline-block pb-2 text-xl md:text-2xl font-bold
+        after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[3px] 
+        after:bg-gradient-to-r after:from-mint-10 after:to-mint-7"
+      >
+        {t("title")}
+      </h2>
+    </SectionWrapper>
+  );
 };
