00ab0876b22d92a340c76838377db573768909c9
Gaetan Redoutez <gaetan.redoutez@preconys.com>
2025-09-17 08:07:51 +0200

    fix(projects): responsive grid

--- src/app/[locale]/components/Projects/Projects.tsx
+++ src/app/[locale]/components/Projects/Projects.tsx
@@ -25,7 +25,7 @@ export const Projects = async () => {
         {t("title")}
       </h2>
       <div className="text-neutral-3">{t("subtitle")}</div>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {projects.map((project, index) => (
           <Card key={index} {...project} />
         ))}
